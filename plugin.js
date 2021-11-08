d7cf90407faef1a61f0cd31642c17bc3f26271b4
winstontom <93392068+winstontom@users.noreply.github.com>
2021-11-08 10:53:55 -0500

    Add files via upload

---
+++ plugin.js
@@ -0,0 +1,20 @@
+	var MyName = "Tom Winston";
+		console.log("Hello from",MyName);
+		console.log("What is your name?");
+	var YourName = "";
+		YourName = prompt ("Enter your name.");
+		document.write(YourName, ", I think that is a nice name.","<br>");
+		console.log(YourName, "Has this many letters", YourName.length);
+		
+	if (YourName = "Abraham Lincoln") {
+		document.write("No it's not, you're dead,");
+	}
+	
+	var FavoriteTeam = "";
+		FavoriteTeam = prompt ("Enter your favorite team.");
+	
+	if (FavoriteTeam = "Tigers") {
+		document.write("<br><h1>","You have good taste in teams!","</h1>");
+	} else {
+		document.write("<br>","Tigers are better.");
+	}
